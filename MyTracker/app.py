7f26dc26d19e266ce86214b2b37dab1e4610215a
narruzza <nickarruzza@gmail.com>
2024-11-25 08:56:19 +1100

    Implement database setup and user management features in MyTracker

--- MyTracker/app.py
+++ MyTracker/app.py
@@ -1,37 +1,27 @@
-# app.py
-from flask import Flask, render_template, request, flash, session, jsonify
-from wekzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, flash, session, redirect, url_for, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import User
+from setup_db import User, ToDo
+from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
 app.secret_key = '69691234'
 
+
+
 # Set up database + session
-engine = create_engine("sqlite:///users.db")
-Session = sessionmaker(build=engine)
+engine = create_engine("sqlite:///todo.db")
+Session = sessionmaker(bind=engine)
 db_session = Session()
 
-# Define user details
-username = "admin"
-password = "admin123"
 
-# Hash the password
-hashed_password = generate_password_hash(password)
 
-# Create a new user instance
-new_user = User(username=username, password=hashed_password)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Add the new user to the database
-db_session.add(new_user)
-db_session.commit()
-print("User added to the database")
 
-# Route for singup page
-@app.route('/signup', methods=["GET", "POST"])
-def signup():
-    return render_template('static', 'signup.html')
 
 # Route for login page
 @app.route('/login', methods=["GET", "POST"])
@@ -39,15 +29,55 @@ def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-
-        # Query the database for the user
         user = db_session.query(User).filter_by(username=username).first()
 
         if user and check_password_hash(user.password, password):
-            flash("You have successfully logged in", "success")
-            session['user_id'] = user.id
+            session["user_id"] = user.id
+            session["username"] = user.username
+            flash("Login Successful", "info")
+            return redirect(url_for("todo_list"))
         else:
             flash("Invalid username or password", "danger")
+    return render_template('login.html')
+
+
+# Route for signup page
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    return render_template('signup.html')
+
+# @app.route('/todo_list')
+# def todo_list():
+#     if "user_id" not in session:
+#         flash("Please login to view that page", "warning")
+#         return redirect(url_for("login"))
+#     return render_template("todo_list.html", user_id=session["user_id"], username=session["username"])
+
+
+# # Google OAuth login route
+# @app.route("/google_login")
+# def google_login():
+#     if not google.authorized:
+#         return redirect(url_for("google.login"))
+
+#     resp = google.get("/plus/v1/people/me")
+#     assert resp.ok, resp.text
+#     email = resp.json()["emails"][0]["value"]
+
+#     # Check if user already exists
+#     user = db_session.query(User).filter_by(username=email).first()
+#     if not user:
+#         # Create a new user if it doesn't exist
+#         new_user = User(username=email, password=generate_password_hash("default_password"))
+#         db_session.add(new_user)
+#         db_session.commit()
+
+#     # Log in the user
+#     session["user_id"] = user.id if user else new_user.id
+#     session["username"] = email
+#     flash("Google login successful!", "info")
+#     return redirect(url_for("todo_list"))
+
 
 # Run the app
 if __name__ == '__main__':
